38c2cafea3cd8485248eb0b8f91735ba32d14034
Dmitry <dmitryesin@hotmail.com>
2025-09-01 11:35:47 +0300

    Initialize Gradle build configuration

---
+++ solver-common/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.5.5"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.solver"
+version = "0.1.2-alpha"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // https://mvnrepository.com/artifact/io.github.cdimascio/java-dotenv
+    implementation("io.github.cdimascio:java-dotenv:5.2.2")
+
+    // https://mvnrepository.com/artifact/net.objecthunter/exp4j
+    implementation("net.objecthunter:exp4j:0.4.8")
+
+    // https://mvnrepository.com/artifact/org.postgresql/postgresql
+    implementation("org.postgresql:postgresql:42.7.5")
+
+    // Spring Boot starters
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "com.solver.Application"
+    }
+}
